9274a791607561ababfcada2b6a75a60533fabaa
Vojislav Stevanović <95634547+pipstur@users.noreply.github.com>
2023-10-13 17:56:11 +0200

    Update clustering_module.py

--- clustering_module.py
+++ clustering_module.py
@@ -6,32 +6,28 @@ from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 
 def perform_clustering(data):
-    # Convert data to DataFrame
     df = pd.DataFrame(data)
-
-    # Drop unwanted columns
     df = df.drop(["customerID", "Churn"], axis=1)
     df = df.dropna(axis=0)
-    # Handle missing data in TotalCharges
+    
     df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
     imputer = SimpleImputer(strategy='mean')
     df['TotalCharges'] = imputer.fit_transform(df[['TotalCharges']])
 
-    # Separate numerical and categorical columns
+    
     numerical_cols = df.select_dtypes(include='number').columns
     categorical_cols = df.select_dtypes(include='object').columns
 
     # Create a column transformer to preprocess numerical and categorical columns
     preprocessor = ColumnTransformer(
         transformers=[
-            ('num', StandardScaler(), numerical_cols),  # Standardize numerical features
-            ('cat', OneHotEncoder(), categorical_cols)  # One-hot encode categorical features
+            ('num', StandardScaler(), numerical_cols),  # Standardize
+            ('cat', OneHotEncoder(), categorical_cols)  # One-hot encode
         ])
-
-    # Create a pipeline with preprocessing and clustering steps
+    
     pipeline = Pipeline([
         ('preprocessor', preprocessor),
-        ('clusterer', KMeans(n_clusters=2, random_state=27))  # Example: KMeans clustering with 3 clusters
+        ('clusterer', KMeans(n_clusters=2, random_state=27))  
     ])
 
     # Fit and predict clusters
